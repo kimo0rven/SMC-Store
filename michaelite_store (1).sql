a41eef7f301d0d3621238d6b8c9fdbdf2d235a98
Twigs <kimorvenvalencia@gmail.com>
2025-09-12 12:24:15 +0800

    db

---
+++ michaelite_store (1).sql
@@ -0,0 +1,458 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 12, 2025 at 06:24 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `michaelite_store`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(100) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `date_modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`category_id`, `category_name`, `date_created`, `date_modified`) VALUES
+(1, 'Test Category', '2025-09-11 20:27:05', '2025-09-11 20:27:05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `listings`
+--
+
+CREATE TABLE `listings` (
+  `listings_id` int(11) NOT NULL,
+  `listing_owner_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `stock_quantity` int(11) NOT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  `category_id` int(11) NOT NULL,
+  `listing_status` enum('active','draft','pending','unavailable','inactive','out of stock','pre-order','discontinued') NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `date_modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `condition` enum('Brand New','Like New','Lightly Used','Well Used','Heavily Used','Refurbished') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `listings`
+--
+
+INSERT INTO `listings` (`listings_id`, `listing_owner_id`, `name`, `description`, `price`, `stock_quantity`, `image_url`, `category_id`, `listing_status`, `date_created`, `date_modified`, `condition`) VALUES
+(2, 1, 'Pegasus 41', 'Experience a new level of running with the Nike Pegasus 41. Featuring a cushioned sole and a lightweight, breathable design, these shoes offer superior comfort and support for daily training runs.', 130.00, 1, 'pegasus_41.jpeg', 1, 'active', '2025-09-11 20:28:31', '2025-09-11 20:36:53', 'Like New'),
+(3, 1, 'asics novablast 5', 'Experience a super-bouncy and comfortable ride with the Asics Novablast 5. Its lightweight foam technology provides a cushioned feel, making it ideal for long runs and daily training.', 120.00, 1, 'asics-novablast-5.jpeg', 1, 'active', '2025-09-11 20:28:52', '2025-09-11 20:30:24', 'Brand New'),
+(4, 1, 'iphone 17', 'The new iPhone 17 features an A-series chip for lightning-fast performance, an enhanced camera system for stunning photos and video, and a sleek new design.', 1199.00, 1, 'iphone_17.jpg', 1, 'active', '2025-09-11 20:28:58', '2025-09-11 20:37:00', 'Refurbished'),
+(5, 1, 'Konu Mini Krunch Cereal', 'Start your day with a satisfying crunch. This 160g box of Konu Mini Krunch Cereal is packed with delicious, bite-sized chocolate puffs that stay crispy in milk.', 5.50, 1, 'konu_mini_krunch_cereal_jar.jpeg', 1, 'active', '2025-09-11 20:29:05', '2025-09-11 20:37:07', 'Heavily Used'),
+(6, 1, 'NoN Raven Cap', 'Stay cool and protected with the lightweight NoN Raven Cap. Its moisture-wicking fabric and classic design make it perfect for running, hiking, or casual wear.', 35.00, 1, 'NoN_Raven_Cap.jpg', 1, 'active', '2025-09-11 20:29:12', '2025-09-11 20:30:49', 'Brand New'),
+(7, 1, 'NoN Running Mom Cap', 'Show off your style with the NoN Running Mom Cap. This stylish and comfortable cap is designed for active moms who want to look great on the go.', 35.00, 1, 'non_running_mom_cap.jpg', 1, 'active', '2025-09-11 20:29:18', '2025-09-11 20:31:00', 'Brand New'),
+(8, 1, 'Pixel 10', 'Discover the power of Google AI with the Pixel 10. Featuring an advanced Tensor chip, an incredible camera system, and a clean Android experience, it’s a smartphone built for the future.', 999.00, 1, 'pixel_10.jpeg', 1, 'active', '2025-09-11 20:29:24', '2025-09-11 20:37:18', 'Lightly Used'),
+(9, 1, 'play station 5', 'Experience lightning-fast loading with an ultra-high-speed SSD, deeper immersion with support for haptic feedback, adaptive triggers, and 3D Audio, and an all-new generation of incredible PlayStation games.', 499.00, 1, 'plash_speed_5_.jpg', 1, 'active', '2025-09-11 20:29:30', '2025-09-11 20:31:30', 'Brand New'),
+(10, 1, 'propel advanced 1', 'Engineered for speed and efficiency, the Propel Advanced 1 is a carbon-fiber road bike with an aerodynamic frame and performance components. Conquer climbs and sprint on flats with ease.', 3500.00, 1, 'propel_advanced_1.jpeg', 1, 'active', '2025-09-11 20:29:35', '2025-09-11 20:31:37', 'Brand New'),
+(12, 1, 'M2 Macbook Air 15 Inch', '10-Core CPU\r\n8-Core GPU\r\n16GB Unified Memory\r\n256GB SSD Storage footnote ¹\r\n16-core Neural Engine\r\n13.6-inch Liquid Retina display with True Tone²\r\n12MP Center Stage camera\r\nMagSafe 3 charging port\r\nTwo Thunderbolt 4 ports\r\nSupport for up to two external displays\r\nMagic Keyboard with Touch ID\r\nForce Touch trackpad\r\n30W USB-C Power Adapter', 64990.00, 1, 'Macbook_Air_15_inch.jpg', 1, 'draft', '2025-09-11 20:35:23', '2025-09-11 20:35:23', 'Well Used');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+
+CREATE TABLE `order` (
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `order_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `total_amount` decimal(10,2) NOT NULL,
+  `status` enum('pending','on hold','processing','shipped','delivered','completed','cancelled') NOT NULL,
+  `shipping_address_id` int(11) NOT NULL,
+  `date_modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `order_id` int(11) NOT NULL,
+  `listing_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `item_total` decimal(10,2) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `date_modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `payment_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  `payment_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `method` enum('cod','paypal') DEFAULT NULL,
+  `transaction_id` varchar(255) DEFAULT NULL,
+  `payment_status` varchar(50) NOT NULL,
+  `date_modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review`
+--
+
+CREATE TABLE `review` (
+  `review_id` int(11) NOT NULL,
+  `listing_id` int(11) NOT NULL,
+  `review_content` text DEFAULT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `date_modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review_image`
+--
+
+CREATE TABLE `review_image` (
+  `review_image_id` int(11) NOT NULL,
+  `review_id` int(11) NOT NULL,
+  `image_file` varchar(255) DEFAULT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `date_modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `role_id` int(11) NOT NULL,
+  `role_name` varchar(100) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `date_modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`role_id`, `role_name`, `date_created`, `date_modified`) VALUES
+(1, 'Admin', '2025-09-11 20:24:16', '2025-09-11 20:24:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shipping_address`
+--
+
+CREATE TABLE `shipping_address` (
+  `shipping_address_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `street_address` varchar(255) DEFAULT NULL,
+  `barangay` varchar(100) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `province` varchar(100) DEFAULT NULL,
+  `type` enum('home','work','pick up') DEFAULT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `date_modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `user_account_id` int(11) NOT NULL,
+  `first_name` varchar(100) DEFAULT NULL,
+  `middle_name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `mobile` varchar(20) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `civil_status` enum('single','married') DEFAULT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `date_modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `user_account_id`, `first_name`, `middle_name`, `last_name`, `mobile`, `gender`, `dob`, `civil_status`, `date_created`, `date_modified`) VALUES
+(1, 1, 'Kim', NULL, 'Valencia', '09355891759', NULL, NULL, 'single', '2025-09-11 20:28:23', '2025-09-11 20:28:23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_account`
+--
+
+CREATE TABLE `user_account` (
+  `user_account_id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `account_status` enum('active','inactive') NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `date_modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_account`
+--
+
+INSERT INTO `user_account` (`user_account_id`, `email`, `password`, `role_id`, `account_status`, `date_created`, `date_modified`) VALUES
+(1, 'kimo0rven@gmail.com', '123', 1, 'active', '2025-09-11 20:24:24', '2025-09-12 02:04:21');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`),
+  ADD UNIQUE KEY `category_name` (`category_name`);
+
+--
+-- Indexes for table `listings`
+--
+ALTER TABLE `listings`
+  ADD PRIMARY KEY (`listings_id`),
+  ADD KEY `fk_listings_owner` (`listing_owner_id`),
+  ADD KEY `fk_listings_category` (`category_id`);
+
+--
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `fk_order_user` (`user_id`),
+  ADD KEY `fk_order_shipping_address` (`shipping_address_id`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`order_id`,`listing_id`),
+  ADD KEY `fk_order_items_listings` (`listing_id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`payment_id`),
+  ADD KEY `fk_payment_order` (`order_id`);
+
+--
+-- Indexes for table `review`
+--
+ALTER TABLE `review`
+  ADD PRIMARY KEY (`review_id`),
+  ADD KEY `fk_review_listings` (`listing_id`);
+
+--
+-- Indexes for table `review_image`
+--
+ALTER TABLE `review_image`
+  ADD PRIMARY KEY (`review_image_id`),
+  ADD KEY `fk_review_image_review` (`review_id`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`role_id`),
+  ADD UNIQUE KEY `role_name` (`role_name`);
+
+--
+-- Indexes for table `shipping_address`
+--
+ALTER TABLE `shipping_address`
+  ADD PRIMARY KEY (`shipping_address_id`),
+  ADD KEY `fk_shipping_address_user` (`user_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_account_id` (`user_account_id`);
+
+--
+-- Indexes for table `user_account`
+--
+ALTER TABLE `user_account`
+  ADD PRIMARY KEY (`user_account_id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `fk_user_account_role` (`role_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `listings`
+--
+ALTER TABLE `listings`
+  MODIFY `listings_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `review`
+--
+ALTER TABLE `review`
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `review_image`
+--
+ALTER TABLE `review_image`
+  MODIFY `review_image_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `shipping_address`
+--
+ALTER TABLE `shipping_address`
+  MODIFY `shipping_address_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user_account`
+--
+ALTER TABLE `user_account`
+  MODIFY `user_account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `listings`
+--
+ALTER TABLE `listings`
+  ADD CONSTRAINT `fk_listings_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`),
+  ADD CONSTRAINT `fk_listings_owner` FOREIGN KEY (`listing_owner_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `fk_order_shipping_address` FOREIGN KEY (`shipping_address_id`) REFERENCES `shipping_address` (`shipping_address_id`),
+  ADD CONSTRAINT `fk_order_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD CONSTRAINT `fk_order_items_listings` FOREIGN KEY (`listing_id`) REFERENCES `listings` (`listings_id`),
+  ADD CONSTRAINT `fk_order_items_order` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`);
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `fk_payment_order` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`);
+
+--
+-- Constraints for table `review`
+--
+ALTER TABLE `review`
+  ADD CONSTRAINT `fk_review_listings` FOREIGN KEY (`listing_id`) REFERENCES `listings` (`listings_id`);
+
+--
+-- Constraints for table `review_image`
+--
+ALTER TABLE `review_image`
+  ADD CONSTRAINT `fk_review_image_review` FOREIGN KEY (`review_id`) REFERENCES `review` (`review_id`);
+
+--
+-- Constraints for table `shipping_address`
+--
+ALTER TABLE `shipping_address`
+  ADD CONSTRAINT `fk_shipping_address_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `fk_user_user_account` FOREIGN KEY (`user_account_id`) REFERENCES `user_account` (`user_account_id`);
+
+--
+-- Constraints for table `user_account`
+--
+ALTER TABLE `user_account`
+  ADD CONSTRAINT `fk_user_account_role` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
